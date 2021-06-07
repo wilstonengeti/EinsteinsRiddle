4065c9be5a0dcc2cf5b3af087f65e2526f0e5bee
wngeti85-creator <63838012+wngeti85-creator@users.noreply.github.com>
2021-06-07 11:59:39 +0300

    initialcommit

---
+++ Einstein/program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace EinsteinsRiddle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Einsteins Riddle");
+            Riddle.Solve();
+            Console.WriteLine("exiting");
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+Solution for Einsteins Riddle
+
+1. Wilstone Ngeti -110606
+2. Raphael Kizaru - 100854
+3. Samson Odour - 110954
+4. Walter Mutuku - 101023
